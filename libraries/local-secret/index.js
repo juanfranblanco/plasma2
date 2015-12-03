978957b7a3c7e77b65f7c3a5690dff3c5ce606f5
James Calfee <james@jcalfee.info>
2015-12-03 13:15:43 -0600

    update

--- libraries/local-secret/index.js
+++ libraries/local-secret/index.js
@@ -5,7 +5,7 @@ const { npm_config__graphene_local_secret_secret } = process.env
 if( ! npm_config__graphene_local_secret_secret ) {
     const buf = crypto.randomBytes(256)
     const local_secret = buf.toString('base64')
-    console.error("# WARN you need to run this command to lock-in your secret."
+    console.error("# WARN you need to run this command to lock-in your secret.")
     console.error("# Alternatively you could add @graphene/local-secret:secret = xxxxx to ./.npmrc")
     console.error("npm config set @graphene/local-secret:secret '%s'", local_secret)
     console.error()
